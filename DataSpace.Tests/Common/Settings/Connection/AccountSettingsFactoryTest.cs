c6c2ead64abade70415614a1a0e64ab64e579fa2
Till Lorentzen <till.lorentzen@graudata.com>
2016-03-24 15:29:32 +0100

    Fixed wrong nunit package description

---
+++ DataSpace.Tests/Common/Settings/Connection/AccountSettingsFactoryTest.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Tests {
+    using System;
+
+    using DataSpace.Common.Settings.Connection;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AccountSettingsFactoryTest {
+        [Test]
+        public void CreateInstance() {
+            IAccountSettingsFactory underTest = new AccountSettingsFactory();
+        }
+    }
+}
