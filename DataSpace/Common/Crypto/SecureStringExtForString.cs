411d505027a4e013b3ea77d258f6647c6924ad17
Till Lorentzen <till.lorentzen@graudata.com>
2016-04-04 13:28:06 +0200

    code style

--- DataSpace/Common/Crypto/SecureStringExtForString.cs
+++ DataSpace/Common/Crypto/SecureStringExtForString.cs
@@ -16,39 +16,31 @@
 //
 // </copyright>
 //-----------------------------------------------------------------------
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Security;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DataSpace.Common.Crypto
-{
+namespace DataSpace.Common.Crypto {
+    ﻿using System;
+    using System.Runtime.InteropServices;
+    using System.Security;
+
     /// <summary>
     /// SecureString Extensions for usage with standart string
     /// </summary>
-    public static class SecureStringExtForString
-    {
+    public static class SecureStringExtForString {
         /// <summary>
         /// converts SecureString in normal String
         /// </summary>
         /// <param name="securePassword">Secure Password object</param>
         /// <returns>Password as normal String</returns>
-        public static string ConvertToUnsecureString(this SecureString securePassword)
-        {
-            if (securePassword == null)
+        public static string ConvertToUnsecureString(this SecureString securePassword) {
+            if (securePassword == null) {
                 throw new ArgumentNullException("securePassword");
+            }
 
             IntPtr unmanagedString = IntPtr.Zero;
-            try
-            {
+            try {
                 unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(securePassword);
                 return Marshal.PtrToStringUni(unmanagedString);
-            }
-            finally
-            {
+            } finally {
                 Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
             }
         }
@@ -58,18 +50,19 @@ namespace DataSpace.Common.Crypto
         /// <param name="securePassword">Secure Password object</param>
         /// <param name="unsecurePassword">Password as normal String</param>
         /// <returns>initialized Secure Password</returns>
-        public static SecureString Init(this SecureString securePassword, string unsecurePassword)
-        {
-            if (securePassword == null)
+        public static SecureString Init(this SecureString securePassword, string unsecurePassword) {
+            if (securePassword == null) {
                 throw new ArgumentNullException("securePassword");
+            }
+
             // Change null to empty Pwd
             unsecurePassword = unsecurePassword ?? string.Empty;
 
             securePassword.Clear();
-            foreach (char Letter in unsecurePassword)
-            {
+            foreach (char Letter in unsecurePassword) {
                 securePassword.AppendChar(Letter);
             }
+
             return securePassword;
         }
     }
