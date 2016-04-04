d9a8ef6f2322a1ada8b163fb63629bb0ed0cd565
Till Lorentzen <till.lorentzen@graudata.com>
2016-04-04 16:38:49 +0200

    Improved code style

--- DataSpace.Tests/Common/Settings/Connection/ProxySettingsTests.cs
+++ DataSpace.Tests/Common/Settings/Connection/ProxySettingsTests.cs
@@ -16,48 +16,40 @@
 //
 // </copyright>
 //-----------------------------------------------------------------------
-﻿using NUnit.Framework;
-using DataSpace.Common.Settings.Connection.W32;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DataSpace.Common.Settings.Connection;
-using System.ComponentModel;
-using DataSpace.Common.Crypto;
-using DataSpace.Common.Utils;
 
-namespace Tests.Common.Settings.Connection
-{
+namespace Tests.Common.Settings.Connection {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+
+    using DataSpace.Common.Crypto;
+    using DataSpace.Common.Settings.Connection;
+    using DataSpace.Common.Settings.Connection.W32;
+    using DataSpace.Common.Utils;
+    using DataSpace.Tests.Utils;
+
+    ﻿using NUnit.Framework;
+
     [TestFixture]
-    public class ProxySettingsTest
-    {
+    public class ProxySettingsTest : WithConfiguredLog4Net {
         private string _Url = "test.url.com";
         private string _UserName = "TestName";
         private string _Password = "TestPassword";
 
-        [TestFixtureSetUp]
-        public void Init()
-        {
-            // calling a logger function triggers reading attributed log4Net settings
-            log4net.LogManager.GetLogger(typeof(ProxySettings));
-        }
         [Test]
-        public void Constructor()
-        {
+        public void Constructor() {
             IProxySettings ProxSet = new ProxySettings();
             // check default values
-            Assert.AreEqual(false, ProxSet.IsDirty);
-            Assert.AreEqual(false, ProxSet.NeedLogin);
-            Assert.AreEqual(ProxyType.None, ProxSet.ProxyType);
-            Assert.AreEqual(string.Empty, ProxSet.Url);
-            Assert.AreEqual(string.Empty, ProxSet.UserName);
-            Assert.AreEqual(string.Empty, ProxSet.Password.ConvertToUnsecureString());
+            Assert.That(ProxSet.IsDirty, Is.False);
+            Assert.That(ProxSet.NeedLogin, Is.False);
+            Assert.That(ProxSet.ProxyType, Is.EqualTo(ProxyType.None));
+            Assert.That(ProxSet.Url, Is.Empty);
+            Assert.That(ProxSet.UserName, Is.Empty);
+            Assert.That(ProxSet.Password.ConvertToUnsecureString(), Is.Empty);
         }
+
         [Test]
-        public void PropertyGetSet()
-        {
+        public void PropertyGetSet() {
             IProxySettings ProxSet = new ProxySettings();
             // act
             ProxSet.Url = _Url;
@@ -66,16 +58,16 @@ namespace Tests.Common.Settings.Connection
             ProxSet.NeedLogin = true;
             ProxSet.ProxyType = ProxyType.Custom;
             // assert
-            Assert.AreEqual(_Url, ProxSet.Url);
-            Assert.AreEqual(_UserName, ProxSet.UserName);
-            Assert.AreEqual(_Password, ProxSet.Password.ConvertToUnsecureString());
-            Assert.AreEqual(true, ProxSet.NeedLogin);
-            Assert.AreEqual(ProxyType.Custom, ProxSet.ProxyType);
-            Assert.AreEqual(true, ProxSet.IsDirty);
+            Assert.That(ProxSet.Url, Is.EqualTo(_Url));
+            Assert.That(ProxSet.UserName, Is.EqualTo(_UserName));
+            Assert.That(ProxSet.Password.ConvertToUnsecureString(), Is.EqualTo(_Password));
+            Assert.That(ProxSet.NeedLogin, Is.True);
+            Assert.That(ProxSet.ProxyType, Is.EqualTo(ProxyType.Custom));
+            Assert.That(ProxSet.IsDirty, Is.True);
         }
+
         [Test]
-        public void CreateNew()
-        {
+        public void CreateNew() {
             IProxySettings ProxSet = new ConnectionSettingsFactory().ProxySettings;
             ProxSet.ProxyType = ProxyType.Custom;
             ProxSet.NeedLogin = true;
@@ -85,14 +77,13 @@ namespace Tests.Common.Settings.Connection
             ProxSet.Save();
         }
 
-        [Test()]
-        public void DeleteTest()
-        {
+        [Test]
+        public void DeleteTest() {
             IProxySettings ProxSet = new ConnectionSettingsFactory().ProxySettings;
 
             ProxSet.Delete();
 
-            Assert.AreEqual(false, ProxSet.IsDirty);
+            Assert.That(ProxSet.IsDirty, Is.False);
             Assert.AreEqual(false, ProxSet.NeedLogin);
             Assert.AreEqual(ProxyType.None, ProxSet.ProxyType);
             Assert.AreEqual(string.Empty, ProxSet.Url);
@@ -100,51 +91,45 @@ namespace Tests.Common.Settings.Connection
             Assert.AreEqual(string.Empty, ProxSet.Password.ConvertToUnsecureString());
         }
 
-        [Test()]
-        public void Load_TriggersEvent()
-        {
+        [Test]
+        public void Load_TriggersEvent() {
             string tstr = AppDomain.CurrentDomain.BaseDirectory;
             IProxySettings ProxSet = new ConnectionSettingsFactory().ProxySettings;
             // Load Event Handler
             bool IsTriggered = false;
-            ProxSet.SettingsLoaded += (sender, arg) =>
-            {
+            ProxSet.SettingsLoaded += (sender, arg) => {
                 IsTriggered = true;
             };
 
             // Load 
             ProxSet.Load();
 
-            Assert.AreEqual(true, IsTriggered);
+            Assert.That(IsTriggered, Is.True);
         }
 
-        [Test()]
-        public void Save_TriggersEvent()
-        {
+        [Test]
+        public void Save_TriggersEvent() {
             IProxySettings ProxSet = new ConnectionSettingsFactory().ProxySettings;
             // Save Event Handler
             bool IsTriggered = false;
-            ProxSet.SettingsSaved += (sender, arg) =>
-            {
+            ProxSet.SettingsSaved += (sender, arg) => {
                 IsTriggered = true;
             };
             // Save 
             ProxSet.Save();
 
-            Assert.AreEqual(true, IsTriggered);
+            Assert.That(IsTriggered, Is.True);
         }
 
-       [Test]
-        public void OnPropertyChanged_Success()
-        {
+        [Test]
+        public void OnPropertyChanged_Success() {
             IProxySettings ProxSet = new ProxySettings();
 
             List<string> ReceivedEvents = new List<string>();
 
-            ProxSet.PropertyChanged += delegate (object sender, PropertyChangedEventArgs args)
-                {
-                    ReceivedEvents.Add(args.PropertyName);
-                };
+            ProxSet.PropertyChanged += delegate (object sender, PropertyChangedEventArgs args) {
+                ReceivedEvents.Add(args.PropertyName);
+            };
             // Act
             ProxSet.ProxyType = ProxyType.Custom;
             ProxSet.NeedLogin = true;
@@ -153,26 +138,24 @@ namespace Tests.Common.Settings.Connection
             ProxSet.Password = new System.Security.SecureString().Init(_Password);
 
             // Assert
-            Assert.AreEqual(true, ProxSet.IsDirty);
-            Assert.AreEqual(ProxyType.Custom, ProxSet.ProxyType);
-            Assert.AreEqual(true, ProxSet.NeedLogin);
-            Assert.AreEqual(_Url, ProxSet.Url);
-            Assert.AreEqual(_UserName, ProxSet.UserName);
-            Assert.AreEqual(_Password, ProxSet.Password.ConvertToUnsecureString());
+            Assert.That(ProxSet.IsDirty, Is.True);
+            Assert.That(ProxSet.ProxyType, Is.EqualTo(ProxyType.Custom));
+            Assert.That(ProxSet.NeedLogin, Is.True);
+            Assert.That(ProxSet.Url, Is.EqualTo(_Url));
+            Assert.That(ProxSet.UserName, Is.EqualTo(_UserName));
+            Assert.That(ProxSet.Password.ConvertToUnsecureString(), Is.EqualTo(_Password));
 
-            Assert.AreEqual(6, ReceivedEvents.Count);               
-            Assert.AreEqual(Property.NameOf((IProxySettings a) => a.ProxyType), ReceivedEvents[0]);        
-            Assert.AreEqual(Property.NameOf((IProxySettings a) => a.IsDirty), ReceivedEvents[1]);          
-            Assert.AreEqual(Property.NameOf((IProxySettings a) => a.NeedLogin), ReceivedEvents[2]);        
-            Assert.AreEqual(Property.NameOf((IProxySettings a) => a.Url), ReceivedEvents[3]);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+            Assert.That(ReceivedEvents.Count, Is.EqualTo(6));
+            Assert.AreEqual(Property.NameOf((IProxySettings a) => a.ProxyType), ReceivedEvents[0]);
+            Assert.AreEqual(Property.NameOf((IProxySettings a) => a.IsDirty), ReceivedEvents[1]);
+            Assert.AreEqual(Property.NameOf((IProxySettings a) => a.NeedLogin), ReceivedEvents[2]);
+            Assert.AreEqual(Property.NameOf((IProxySettings a) => a.Url), ReceivedEvents[3]);
             Assert.AreEqual(Property.NameOf((IProxySettings a) => a.UserName), ReceivedEvents[4]);
             Assert.AreEqual(Property.NameOf((IProxySettings a) => a.Password), ReceivedEvents[5]);
-
         }
-        [Test]
-        [NUnit.Framework.Category("Slow")]
-        public void PropGet_TriggersLoad()
-        {
+
+        [Test, NUnit.Framework.Category("Slow")]
+        public void PropGet_TriggersLoad() {
             //prep
             IProxySettings ProxSet = new ConnectionSettingsFactory().ProxySettings;
             // clear dirty flag
@@ -180,8 +163,7 @@ namespace Tests.Common.Settings.Connection
             ProxySettings ProxSetObj = ProxSet as ProxySettings;
             ProxSetObj.PropsRefreshSpan = new TimeSpan(0, 0, 5);
             bool IsTriggered = false;
-            ProxSet.SettingsLoaded += (sender, arg) =>
-            {
+            ProxSet.SettingsLoaded += (sender, arg) => {
                 IsTriggered = true;
             };
             // act
@@ -189,15 +171,13 @@ namespace Tests.Common.Settings.Connection
             System.Threading.Thread.Sleep(6020);
             Url = ProxSet.Url;
             // assert
-            Assert.AreEqual(true, IsTriggered);
+            Assert.That(IsTriggered, Is.True);
             // house keeping
             ProxSetObj.PropsRefreshSpan = new TimeSpan(0, 2, 0);
-
         }
-        [Test]
-        [NUnit.Framework.Category("Slow")]
-        public void PropGet_NoTriggersLoadInRefreshSpan()
-        {
+
+        [Test, NUnit.Framework.Category("Slow")]
+        public void PropGet_NoTriggersLoadInRefreshSpan() {
             // prep
             IProxySettings ProxSet = new ConnectionSettingsFactory().ProxySettings;
             // clear dirty flag
@@ -205,8 +185,7 @@ namespace Tests.Common.Settings.Connection
             ProxySettings ProxSetObj = ProxSet as ProxySettings;
             ProxSetObj.PropsRefreshSpan = new TimeSpan(0, 2, 0);
             bool IsTriggered = false;
-            ProxSet.SettingsLoaded += (sender, arg) =>
-            {
+            ProxSet.SettingsLoaded += (sender, arg) => {
                 IsTriggered = true;
             };
             // act
@@ -214,12 +193,11 @@ namespace Tests.Common.Settings.Connection
             System.Threading.Thread.Sleep(500);
             Url = ProxSet.Url;
             // assert
-            Assert.AreEqual(false, IsTriggered);
+            Assert.That(IsTriggered, Is.False);
         }
-        [Test]
-        [NUnit.Framework.Category("Slow")]
-        public void PropGet_NoTriggersLoadDisabled()
-        {
+
+        [Test, NUnit.Framework.Category("Slow")]
+        public void PropGet_NoTriggersLoadDisabled() {
             // prep
             IProxySettings ProxSet = new ConnectionSettingsFactory().ProxySettings;
             // clear dirty flag
@@ -230,8 +208,7 @@ namespace Tests.Common.Settings.Connection
             // set disabled
             ProxSetObj.PropsRefreshSpan = new TimeSpan(0, 0, 0);
             bool IsTriggered = false;
-            ProxSet.SettingsLoaded += (sender, arg) =>
-            {
+            ProxSet.SettingsLoaded += (sender, arg) => {
                 IsTriggered = true;
             };
             // act
@@ -239,14 +216,13 @@ namespace Tests.Common.Settings.Connection
             System.Threading.Thread.Sleep(5020);
             Url = ProxSet.Url;
             // assert
-            Assert.AreEqual(false, IsTriggered);
+            Assert.That(IsTriggered, Is.False);
             // house keeping
             ProxSetObj.PropsRefreshSpan = new TimeSpan(0, 2, 0);
         }
-        [Test]
-        [NUnit.Framework.Category("Slow")]
-        public void PropGet_NoTriggersLoadEditMode()
-        {
+
+        [Test, NUnit.Framework.Category("Slow")]
+        public void PropGet_NoTriggersLoadEditMode() {
             // prep
             IProxySettings ProxSet = new ConnectionSettingsFactory().ProxySettings;
             // clear dirty flag
@@ -255,8 +231,7 @@ namespace Tests.Common.Settings.Connection
             // set 5 sec timer
             ProxSetObj.PropsRefreshSpan = new TimeSpan(0, 0, 5);
             bool IsTriggered = false;
-            ProxSet.SettingsLoaded += (sender, arg) =>
-            {
+            ProxSet.SettingsLoaded += (sender, arg) => {
                 IsTriggered = true;
             };
             // act
@@ -266,7 +241,7 @@ namespace Tests.Common.Settings.Connection
             System.Threading.Thread.Sleep(5020);
             Url = ProxSet.Url;
             // assert
-            Assert.AreEqual(false, IsTriggered);
+            Assert.That(IsTriggered, Is.False);
             // house keeping
             ProxSetObj.PropsRefreshSpan = new TimeSpan(0, 2, 0);
         }
