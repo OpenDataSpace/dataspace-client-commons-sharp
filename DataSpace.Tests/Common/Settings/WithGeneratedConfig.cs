0e873194b981c90d2fc145cced32883e6c948ea2
Till Lorentzen <till.lorentzen@graudata.com>
2016-04-18 09:27:59 +0200

    Added helper class for configuration file tests

---
+++ DataSpace.Tests/Common/Settings/WithGeneratedConfig.cs
@@ -0,0 +1,45 @@
+//-----------------------------------------------------------------------
+// <copyright file="WithGeneratedConfig.cs" company="GRAU DATA AG">
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General private License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General private License for more details.
+//
+//   You should have received a copy of the GNU General private License
+//   along with this program. If not, see http://www.gnu.org/licenses/.
+//
+// </copyright>
+//-----------------------------------------------------------------------
+﻿
+namespace Tests.Common.Settings {
+    using System;
+    using System.Configuration;
+    using System.IO;
+
+    using DataSpace.Common.Settings;
+    using DataSpace.Tests.Utils;
+
+    using NUnit.Framework;
+
+    public class WithGeneratedConfig : WithConfiguredLog4Net {
+        protected Configuration config;
+
+        [SetUp]
+        public void CreateConfig() {
+            this.config = new ConfigurationLoader(new UserConfigPathBuilder {FileName = Guid.NewGuid().ToString()}).Configuration;
+        }
+
+        [TearDown]
+        public void CleanUp() {
+            if (config.HasFile) {
+                File.Delete(config.FilePath);
+            }
+        }
+    }
+}
